62f18d6628dfe437fd21328b9ae3017e4ca06984
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2026-05-03 01:36:44 +0530

    initial commit - RavisOptical

--- goeye-clone/vite.config.js
+++ goeye-clone/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ravisoptical/',   // must match your GitHub repo name exactly
+  base: '/ravisoptical-/',   // matches your GitHub repo name
 })
